1973d573c8fc76370947a94d876a99da903b48b0
kshitij garg <kshitij.2054@gmail.com>
2022-06-01 15:49:03 +1000

    Refactor business logic, separated methods, make code clear

--- src/main/java/com/example/siesmaTest/SiesmaBusinessLogicLayer.java
+++ src/main/java/com/example/siesmaTest/SiesmaBusinessLogicLayer.java
@@ -1,35 +1,57 @@
 package com.example.siesmaTest;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 public class SiesmaBusinessLogicLayer {
-    //Contains Business Logic
-    public void calculateTaxOfEmployee(Employee emp, ArrayList<TaxSlabPOJO> taxSlabs){
-        int slabCounter = 0;
-        while(emp.getAnnualSalary()>taxSlabs.get(slabCounter).getSlabMax() && slabCounter<taxSlabs.size()){
-            slabCounter++;
-        }
-        if(taxSlabs.get(slabCounter).getSlabMax()==0)//and we see if slab is 0, we set income tax as 0
-            emp.setIncomeTax(0);
-        else {//else we compute tax on money other than passed slab(which will be j-1 here)
-            emp.setIncomeTax(emp.getIncomeTax() + ((emp.getAnnualSalary() - taxSlabs.get(slabCounter).getSlabMin()) * taxSlabs.get(slabCounter).getUnitTax()));
-            emp.setIncomeTax(emp.getIncomeTax() + taxSlabs.get(slabCounter).getSlabBaseTax());//and adding constant tax slab of passed slab
-        }
 
+    public void calculateTax(Employee emp, ArrayList<TaxSlabPOJO> taxSlabs, int incomeSlabOfEmployee) {
+
+       double taxableSalaryInSlab = emp.getAnnualSalary() - taxSlabs.get(incomeSlabOfEmployee).getSlabMin();
+       double taxOnSlab = taxSlabs.get(incomeSlabOfEmployee).getUnitTax();
+       emp.setIncomeTax(emp.getIncomeTax() + taxableSalaryInSlab * taxOnSlab);
+       double baseTaxOnSlab = taxSlabs.get(incomeSlabOfEmployee).getSlabBaseTax();
+       //Add Slab's Base constant tax
+        emp.setIncomeTax(emp.getIncomeTax() + baseTaxOnSlab);
+    }
+
+
+    //Contains Business Logic
+    public void calculateTaxOfEmployee(Employee emp, ArrayList<TaxSlabPOJO> taxSlabs) {
+        int incomeSlabOfEmployee = findIncomeSlabOfEmployee(emp, taxSlabs);
+
+        calculateTax(emp, taxSlabs, incomeSlabOfEmployee);
+
+        setEmployeeDetails(emp);
+        Calendar cal = Calendar.getInstance();
+        emp.setFromDate(getFirstDayMonMonth(cal));
+        emp.setToDate(getLastDayMonMonth(cal));
+    }
+
+
+    public int findIncomeSlabOfEmployee(Employee emp, ArrayList<TaxSlabPOJO> taxSlabs) {
+        int incomeSlabOfEmployee = 0; //Starting from first tax slab
+        while (emp.getAnnualSalary() > taxSlabs.get(incomeSlabOfEmployee).getSlabMax() && incomeSlabOfEmployee < taxSlabs.size()) {
+            incomeSlabOfEmployee++;
+        }
+        return incomeSlabOfEmployee;
+    }
+
+
+    void setEmployeeDetails(Employee emp) {
         emp.setGrossIncome(Math.round(emp.getPaymentMonth() * (emp.getAnnualSalary() / 12)));
         emp.setIncomeTax(Math.round((emp.getIncomeTax() / 12) * emp.getPaymentMonth()));//according to payment months
         emp.setNetIncome(Math.round(emp.getGrossIncome() - emp.getIncomeTax()));
         emp.setSuperannuation(Math.round(emp.getGrossIncome() * emp.getSuperRate()));
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.DATE, cal.getActualMinimum(Calendar.DATE));
-        Date firstDayOfMonth = cal.getTime();
-        emp.setFromDate(firstDayOfMonth);
-        cal.set(Calendar.DATE, emp.getPaymentMonth() * cal.getActualMaximum(Calendar.DATE));//confirm i am multiplying the payment months with days what if feb
-        Date lastDayOfMonth = cal.getTime();
-        emp.setToDate(lastDayOfMonth);
-
     }
+
+    public Date getFirstDayMonMonth(Calendar cal) {
+        cal.set(Calendar.DATE, cal.getActualMinimum(Calendar.DATE));
+        return cal.getTime();
+    }
+
+    public Date getLastDayMonMonth(Calendar cal) {
+        cal.set(Calendar.DATE, cal.getActualMaximum(Calendar.DATE));
+        return cal.getTime();
+    }
+
 }
--- src/main/java/com/example/siesmaTest/SiesmaS3Connector.java
+++ src/main/java/com/example/siesmaTest/SiesmaS3Connector.java
@@ -5,6 +5,7 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -14,8 +15,10 @@ import com.amazonaws.services.s3.model.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.util.StreamUtils;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
@@ -34,13 +37,17 @@ public class SiesmaS3Connector {
 //                    e);
 //        }
 
+     //   BufferedInputStream in = new BufferedInputStream(new URL("http://localhost:4566/siesmataxslabs/taxSlabs.json").openStream());
+
         AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+               // .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566/","us-west-2"))
               //  .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion("us-west-2")
                 .build();
 
-        String s3ObjectAsString = s3.getObjectAsString("siesmataxslabs", "taxSlabs.json");
+         String s3ObjectAsString =  s3.getObjectAsString("siesmataxslabs", "taxSlabs.json");
 
-        return s3ObjectAsString;
+        //      return in.toString();
+        return  s3ObjectAsString;
     }
 }
